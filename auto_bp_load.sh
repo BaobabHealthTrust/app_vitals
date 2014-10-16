c48a3f4c3b9d6260efa4aba1a1a1bb8f7aff0066
Fchirwa <fuvu.chirwa@gmail.com>
2014-10-16 13:48:08 +0200

    Managing message diplay

--- auto_bp_load.sh
+++ auto_bp_load.sh
@@ -2,12 +2,12 @@
 cd /var/www/app_vitals
 
 if [ -f /dev/ttyUSB0 ] && [ -f /dev/ttyS0 ]; then
-	./vitalsserver -b /dev/ttyS0 -s /dev/ttyUSB0 &
+	sudo ./vitalsserver -b /dev/ttyS0 -s /dev/ttyUSB0 &
 elif [ -f /dev/ttyUSB0 ]; then
-	./vitalsserver -b /dev/ttyUSB0 &
+	sudo ./vitalsserver -b /dev/ttyUSB0 &
 elif [ -f /dev/ttyS0 ]; then
-	./vitalsserver -b /dev/ttyS0 &
+	sudo ./vitalsserver -b /dev/ttyS0 &
 else
-	./vitalsserver &
+	sudo ./vitalsserver &
 fi
 #firefox 192.168.18.213:3012
